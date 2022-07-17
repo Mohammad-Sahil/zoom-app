3e883ac70d84ec8e557290cc98ca089d70d946ba
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-07-17 17:54:33 +0530

    expo init

---
+++ screens/Home.js
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+      <Text>Hello Sahil</Text>
+    </View>
+  )
+}
